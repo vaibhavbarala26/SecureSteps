7ec5d1d76efa858aec503bae4f9e851010e6881f
vaibhavbarala26 <157816562+vaibhavbarala26@users.noreply.github.com>
2024-09-28 19:50:56 +0530

    Update SMS.js

--- Controller/SMS.js
+++ Controller/SMS.js
@@ -1,14 +1,14 @@
-// const twilio = require("twilio");
-// const accountSid = process.env.Sid;
-// const authToken = process.env.auth_token;
-// const client = twilio(accountSid, authToken);
-// async function createMessage(numbers) {
-//   numbers.forEach(async(number)=>{
-//     await client.messages.create({
-//       body: "hi boys",
-//       from: "+12565874362",
-//       to: number,
-//   })
-//   })
-// }
-// module.exports = createMessage;  
+const twilio = require("twilio");
+const accountSid = process.env.Sid;
+const authToken = process.env.auth_token;
+const client = twilio(accountSid, authToken);
+async function createMessage(numbers) {
+  numbers.forEach(async(number)=>{
+    await client.messages.create({
+      body: "hi boys",
+      from: "+12565874362",
+      to: number,
+  })
+  })
+}
+module.exports = createMessage;  
