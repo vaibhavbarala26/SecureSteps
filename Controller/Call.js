3d09b25aad3ede1021a7854bfea4d3d26b01568d
vaibhavbarala26 <157816562+vaibhavbarala26@users.noreply.github.com>
2024-09-28 19:50:25 +0530

    Update Call.js

--- Controller/Call.js
+++ Controller/Call.js
@@ -1,15 +1,15 @@
-// const twilio = require("twilio"); 
-// const accountSid = process.env.Sid;
-// const authToken = process.env.auth_token;
-// const client = twilio(accountSid, authToken);
-// async function createCall(numbers) {
-//   numbers.forEach(async(number)=>{
-//     await client.calls.create({
-//       from: "+12565874362",
-//       to: number,
-//     twiml: "<Response><Say>Teri Maa ki chut</Say></Response>",
-//   })
-//   .then((call)=>console.log(call.sid))
-//   })
-//   }
-// module.exports = createCall;
+const twilio = require("twilio"); 
+const accountSid = process.env.Sid;
+const authToken = process.env.auth_token;
+const client = twilio(accountSid, authToken);
+async function createCall(numbers) {
+  numbers.forEach(async(number)=>{
+    await client.calls.create({
+      from: "+12565874362",
+      to: number,
+    twiml: "<Response><Say>Teri Maa ki chut</Say></Response>",
+  })
+  .then((call)=>console.log(call.sid))
+  })
+  }
+module.exports = createCall;
